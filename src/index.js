a0c4a104ad289c48d5e1f7657916120779577cfa
Carl Mungazi <carl.mungazi@airfinetworks.com>
2019-02-19 20:07:19 +0000

    Create example application for testing react scheduling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import ReactExperimentalRenderer from './renderer';
+import ReactDOM from 'react-dom';
+// import ReactExperimentalRenderer from './renderer';
 
 const Text = props => {
   return <p className={props.className}>{props.content}</p>
@@ -8,23 +8,44 @@ const Text = props => {
 
 class App extends React.Component {
   state = {
-    text: Date.now(),
+    inputVal: "",
+    companyName: "",
+    showAnimation: false
   }
 
-  onButtonClick = () => {
-    this.setState(() => ({ text: Date.now() }))
+  handleInputChange = (e) => {
+    this.makeApiCall();
+    this.setState({ inputVal: e.target.value, showAnimation: true });
+  }
+
+  makeApiCall = () => {
+    return fetch('https://api.iextrading.com/1.0/stock/aapl/company')
+    .then(res => res.json())
+    .then(data => this.setState({ companyName: data.companyName }) )
   }
 
   render() {
+    const { inputVal, companyName, showAnimation } = this.state;
     return (
       <div>
-        <Text className="hello-class" content={this.state.text} />
-        <span style="color: blue;" autofocus>World</span>
-        <button onClick={this.onButtonClick}>Get Current Time</button>
+        <Text className="input-data" content={`Input Text: ${inputVal}`} />
+        <Text className="api-data" content={`API Data: ${companyName}`} />
+        <input id="input" type="text" value={inputVal} onChange={this.handleInputChange}  />
+        { showAnimation && 
+          <div 
+            className="animation"
+            style={{
+              'width': '50px',
+              'height': '50px',
+              'backgroundColor': 'red',
+              'marginTop': '10px'
+            }}
+          />
+        }
       </div>
     )
   }
 }
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactExperimentalRenderer.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactExperimentalRenderer.render(<App />, document.getElementById('root'));
