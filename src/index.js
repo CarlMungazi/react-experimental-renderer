e5632a94f8372e119bfa080887be1210af673630
Carl Mungazi <carl.mungazi@airfinetworks.com>
2019-03-14 20:04:36 +0000

    Begin React.Component implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+// import React from 'react';
 // import ReactDOM from 'react-dom';
+
+import React from './react';
 import ReactExperimentalRenderer from './renderer';
+import Input from 'react-experimental-input/Input';
 
 const Text = props => {
   return <p className={props.className}>{props.content}</p>
@@ -9,10 +12,15 @@ const Text = props => {
 class App extends React.Component {
   state = {
     text: Date.now(),
+    inputText: ""
   }
 
   onButtonClick = () => {
     this.setState(() => ({ text: Date.now() }))
+  }
+
+  onInputChange = (e) => {
+    this.setState({ inputText: e.target.value });
   }
 
   render() {
@@ -21,9 +29,18 @@ class App extends React.Component {
         <Text className="hello-class" content={this.state.text} />
         <span style="color: blue;" autofocus>World</span>
         <button onClick={this.onButtonClick}>Get Current Time</button>
+        <Input 
+          value={this.state.inputText}
+          onChange={ e => this.onInputChange(e.target.value)}
+          placeholder="Enter your input"
+          id="class-input"
+        />
       </div>
     )
   }
 }
 
-ReactExperimentalRenderer.render(<App />, document.getElementById('root'));
+ReactExperimentalRenderer.render(
+  <App />, 
+  document.getElementById('root')
+);
